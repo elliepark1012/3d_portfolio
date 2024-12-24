792949817ebb665796bbeb170fc00276c59efee0
Ellie Park <elliepark@Ellies-MacBook-Pro.local>
2024-12-23 22:31:50 -0500

    about section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -20,10 +21,13 @@ const ServiceCard = ({ index, title, icon }) => {
             scale: 1,
             speed: 450
           }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
-          flex justify-evenly items-center flex-col"
+          className="bg-tertiary rounded-[20px] py-5 px-12 
+          min-h-[280px] flex justify-evenly items-center flex-col"
         >
-
+          <img src={icon} alt={title} 
+        className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold
+          text-center">{title}</h3>
         </div>
 
       </motion.div>
@@ -57,4 +61,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
